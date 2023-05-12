8ab8df9e6248252f0e07960cb7c7702e99d45b1d
Yuriy PC <yurchest@gmail.com>
2023-05-13 01:58:18 +0300

    commit

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import logging
 import asyncio
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ParseMode
-from src.database import *
+from database import *
 
 import json
 
